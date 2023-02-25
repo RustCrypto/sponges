0829350cc7597715120629b79f0cb42eb23360c6
Tony Arcieri <bascule@gmail.com>
2023-02-25 15:26:54 -0700

    ascon: remove `byteorder` dependency
    We can use the support in `core` instead

--- ascon/src/util.rs
+++ ascon/src/util.rs
@@ -1,17 +1,16 @@
-use byteorder::{BigEndian, ByteOrder};
-
-pub type Endian = BigEndian;
+use core::convert::TryInto;
 
 #[inline]
+#[allow(clippy::unwrap_used)]
 pub fn u8_to_u64(input: &[u8], output: &mut [u64]) {
     for (i, b) in output.iter_mut().enumerate() {
-        *b = Endian::read_u64(&input[(i * 8)..((i + 1) * 8)]);
+        *b = u64::from_be_bytes(input[(i * 8)..((i + 1) * 8)].try_into().unwrap());
     }
 }
 
 #[inline]
 pub fn u64_to_u8(input: &[u64], output: &mut [u8]) {
     for (i, &b) in input.iter().enumerate() {
-        Endian::write_u64(&mut output[(i * 8)..((i + 1) * 8)], b)
+        output[(i * 8)..((i + 1) * 8)].copy_from_slice(&b.to_be_bytes())
     }
 }
