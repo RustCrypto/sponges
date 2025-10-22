2a2a9ee7210676261ae8c46a08a71204bb7a687a
Alexandr Kitaev <makavity231@gmail.com>
2025-10-22 20:20:55 +0300

    `belt-bash`: add benches and no_unroll

---
+++ belt-bash/benches/mod.rs
@@ -0,0 +1,38 @@
+#![feature(test)]
+extern crate test;
+
+use belt_bash::bash_f;
+use test::Bencher;
+
+const STATE_WORDS: usize = 24;
+
+#[bench]
+fn bench_bash_f(b: &mut Bencher) {
+    let mut state = [0u64; STATE_WORDS];
+    b.iter(|| {
+        bash_f(test::black_box(&mut state));
+        test::black_box(&state);
+    });
+}
+
+#[bench]
+fn bench_bash_f_10_rounds(b: &mut Bencher) {
+    let mut state = [0u64; STATE_WORDS];
+    b.iter(|| {
+        for _ in 0..10 {
+            bash_f(test::black_box(&mut state));
+        }
+        test::black_box(&state);
+    });
+}
+
+#[bench]
+fn bench_bash_f_100_rounds(b: &mut Bencher) {
+    let mut state = [0u64; STATE_WORDS];
+    b.iter(|| {
+        for _ in 0..100 {
+            bash_f(test::black_box(&mut state));
+        }
+        test::black_box(&state);
+    });
+}
